be7eb3ce44ca249ff61f241bdd63ed0dd1da06cc
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-30 19:23:24 -0300

    Feat: Adicionando mural de listagem de pensamentos.

---
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.scss']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
