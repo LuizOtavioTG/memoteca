d38808742a34859ffb28d8bdb0a9cf6ead64dd85
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-31 09:52:25 -0300

    Feat: Adicionando diretiva estrutural para renderizar todos os pensamentos da lista.

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos = [];
   constructor() { }
 
   ngOnInit(): void {
