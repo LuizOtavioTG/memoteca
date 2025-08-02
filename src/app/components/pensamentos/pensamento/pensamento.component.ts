310acde120c7e50da51aa64a2277aa18f3b44656
LuizOtavioTG <bc.luizo@gmail.com>
2025-08-01 23:57:55 -0300

    Feat: Adicionando edição de pensamentos registrados.

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -8,7 +8,12 @@ import { Pensamento } from 'src/app/interfaces/pensamento';
 })
 export class PensamentoComponent implements OnInit {
 
-  @Input() pensamento!: Pensamento
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'usuario',
+    modelo: 'modelo3'
+  }
   constructor() { }
 
   larguraPensamento(): string {
