c1d9196f0bbffb8226153f99c785c42e6cd4ed82
LuizOtavioTG <bc.luizo@gmail.com>
2025-08-03 23:00:08 -0300

    Feat: Adicionando form reativo e validações em editar pensamento.

--- src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pensamento } from 'src/app/interfaces/pensamento';
 import { PensamentoService } from 'src/app/services/pensamento.service';
@@ -10,19 +11,31 @@ import { PensamentoService } from 'src/app/services/pensamento.service';
 })
 export class EditarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  } as Pensamento;
+  editarPensamentoForm!: FormGroup;
+  minLengthAutoria = 2;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.editarPensamentoForm = this.fb.group({
+      id: [''],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(this.minLengthAutoria)
+      ])],
+      modelo: ['']
+    });
+
+
     this.route.paramMap.subscribe((params) => {
       const id = Number(params.get('id'));
       if (isNaN(id)) {
@@ -31,13 +44,13 @@ export class EditarPensamentoComponent implements OnInit {
       }
 
       this.service.buscarPorId(id).subscribe((pensamento) => {
-        this.pensamento = pensamento;
+        this.editarPensamentoForm.patchValue(pensamento);
       });
-    })
+    });
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => {
+    this.service.editar(this.editarPensamentoForm.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
 
@@ -47,4 +60,10 @@ export class EditarPensamentoComponent implements OnInit {
     this.router.navigate(['/listarPensamento'])
   }
 
+  habilitarBotao(): string {
+    if (this.editarPensamentoForm.valid) {
+      return "botao"
+    }
+    else return "botao__desabilitado"
+  }
 }
